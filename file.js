d002b8d2b76b6ed11dab52b4bf98d8490ccb7fb3
CyntiBinti <cynthia_m@live.com>
2021-08-21 10:35:26 +0100

    updated file

---
+++ file.js
@@ -0,0 +1,12 @@
+function computerPlay {
+    let options = ["Rock", "Paper", "Scissors"];
+    let randomAnswer = options[Math.floor(math.random()*options.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+    
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
