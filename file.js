cc25986cb76a4d147ff75508200e496b806520a2
CyntiBinti <cynthia_m@live.com>
2021-08-23 19:15:37 +0100

    updated file.js

--- file.js
+++ file.js
@@ -1,8 +1,17 @@
-function computerPlay () {
+function computerSelection () {
     let options = ["Rock", "Paper", "Scissors"];
     let randomAnswer = options[Math.floor(Math.random()*options.length)];
     return randomAnswer;
 }
 
-console.log(computerPlay());
+var playerSelection = prompt("Type either 'Rock', 'Paper', or 'Scissors' to play");
 
+function game() {
+    if (playerSelection == "Rock" && computerSelection == "Paper") {
+        console.log("You Lose!");
+    } else if (playerSelection == "Rock" && computerSelection == "Rock") {
+        console.log("It's a Tie!");
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        console.log("You Win!");
+    }
+}
